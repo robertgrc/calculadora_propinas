d11bab7957e0a4ce5e4c985675fc9106cddfb7f8
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-04-26 14:41:07 -0400

    corrigiendo estilos

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../helpers"
 import { OrderItem } from "../types"
 
 
@@ -10,15 +11,30 @@ export default function OrderContents({order}:OrderContentsProps) {
   return (
     <div>
         <h2 className='font-black text-4xl'>Consumo</h2>
-        <div className="space-y-3 mt-5">
+        <div className="space-y-3 mt-10">
             {order.length ===0 ?
                 <p className="text-center">La orden esta vacia</p>
             :(
                 order.map(item => (
-                    <div key={item.id}>
-                        <p>
-                            {item.name} - {item.price}
-                        </p>
+                    <div 
+                      key={item.id} 
+                      className="flex justify-between border-t items-center border-gray-200 py-5 last-of-type:border-b"
+                    >
+                        <div>
+                          <p className="text-lg">
+                              {item.name} - {formatCurrency(item.price)}
+                          </p>
+                          <p className="font-black">
+                              Cantidad:{item.quantity} -{formatCurrency(item.price * item.quantity)}
+                          </p>
+                        </div>
+                    <div>
+                        <button
+                            className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
+                        >
+                            X
+                        </button>
+                    </div>
                     </div>
                 ))
             )
